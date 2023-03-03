ba6335250795e62c7bf8a8fe72e632819640b03d
Timo Sarkar <71646577+timosarkar@users.noreply.github.com>
2023-03-03 19:52:08 +0100

    Update index.ps1

--- index.ps1
+++ index.ps1
@@ -1,3 +1,15 @@
 # add SIP filter
 pktmon filter add -p 20
-pktmon start --etw
+
+# logs traces dynamically in a etl file
+pktmon start --etw -p 0 -l real-time 
+
+# parse etl into txt
+pktmon etl2txt -o ./trace.txt
+
+# if trace contains sip packets
+$string = select-string -path ./trace.txt -pattern 20,'SIP'
+
+if ($string -ne $null) {
+  # terminate spotify audio through api
+}
